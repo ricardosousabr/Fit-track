461b71047b57149d48b3fc91d9700641413ce1cc
ricardosousabr <ricardo.br.pi@gmail.com>
2026-06-03 18:03:32 -0300

    firts commit

---
+++ src/test/java/com/bodyTraining/fittrack/FittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodyTraining.fittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
