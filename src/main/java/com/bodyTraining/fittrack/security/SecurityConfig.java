b32908f208a1b03bc37077411efe7d894e5b2052
ricardosousabr <ricardo.br.pi@gmail.com>
2026-06-17 23:12:19 -0300

    feat: "add security config"

---
+++ src/main/java/com/bodyTraining/fittrack/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.bodyTraining.fittrack.security;
+
+import com.bodyTraining.fittrack.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final UserRepository userRepository;
+	
+	public SecurityConfig (JwtAuthenticationFilter jwtAuthenticationFilter, UserRepository userRepository) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.userRepository = userRepository;
+	}
+	
+	@Bean
+	public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/auth/**").permitAll()
+						.anyRequest().authenticated())
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider())
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return email -> userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+}
