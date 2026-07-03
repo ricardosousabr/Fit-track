bf33b9981490ce44933a6d0580b3adf8d8ebe9a3
ricardosousabr <ricardo.br.pi@gmail.com>
2026-07-02 23:18:41 -0300

    feat: "add controller"

---
+++ src/main/java/com/bodyTraining/fittrack/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.bodyTraining.fittrack.controller;
+
+import com.bodyTraining.fittrack.dto.request.LoginRequest;
+import com.bodyTraining.fittrack.dto.request.RegisterRequest;
+import com.bodyTraining.fittrack.dto.response.AuthResponse;
+import com.bodyTraining.fittrack.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+	private final AuthService authService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+		AuthResponse response = authService.register(request);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+		AuthResponse response = authService.login(request);
+		
+		return ResponseEntity.ok(response);
+	}
+}
