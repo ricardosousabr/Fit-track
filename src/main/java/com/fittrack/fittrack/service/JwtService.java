9087f0b669cdbcccc519b6a14122486229356fde
ricardosousabr <ricardo.br.pi@gmail.com>
2026-07-08 19:39:15 -0300

    feat: "add jwt service"

---
+++ src/main/java/com/fittrack/fittrack/service/JwtService.java
@@ -0,0 +1,56 @@
+package com.fittrack.fittrack.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+	private final SecretKey secretKey;
+	private final long expiration;
+	
+	public JwtService(
+			@Value("${jwt.secret}") String secret,
+			@Value("${jwt.expiration}") long expiration
+	) {
+		this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+		this.expiration = expiration;
+	}
+	
+	public String generateToken(UserDetails userDetails) {
+		return Jwts.builder()
+				.subject(userDetails.getUsername())
+				.issuedAt(new Date())
+				.expiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(secretKey)
+				.compact();
+	}
+	
+	public String extractEmail(String token) {
+		return parseClaims(token).getSubject();
+	}
+	
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		String userName = extractEmail(token);
+		return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return parseClaims(token).getExpiration().before(new Date());
+	}
+	
+	private Claims parseClaims(String token) {
+		return  Jwts.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+}
