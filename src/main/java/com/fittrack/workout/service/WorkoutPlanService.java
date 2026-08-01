ed9af6398bbef1df745c04ddab610481908c0aba
ricardosousabr <ricardo.br.pi@gmail.com>
2026-07-31 21:09:48 -0300

    feat: "add functions"

--- src/main/java/com/fittrack/workout/service/WorkoutPlanService.java
+++ src/main/java/com/fittrack/workout/service/WorkoutPlanService.java
@@ -8,9 +8,14 @@ import com.fittrack.workout.dto.WorkoutPlanResponse;
 import com.fittrack.workout.mapper.WorkoutPlanMapper;
 import com.fittrack.workout.repository.WorkoutPlanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +31,12 @@ public class WorkoutPlanService {
 		return user;
 	}
 	
+	private void checkOwnership(WorkoutPlan record, User user) {
+		if (!record.getUser().getId().equals(user.getId())) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied");
+		}
+	}
+	
 	public WorkoutPlanResponse create(WorkoutPlanRequest request) {
 		User user = getUserAuthenticated();
 		WorkoutPlan workoutPlan = new WorkoutPlan();
@@ -36,10 +47,49 @@ public class WorkoutPlanService {
 		workoutPlan.setStartDate(request.startDate());
 		workoutPlan.setEndDate(request.endDate());
 		
-		
 		workoutPlanRepository.save(workoutPlan);
 		
 		return workoutPlanMapper.toWorkoutPlanResponse(workoutPlan);
 		
+	}
+	
+	public List<WorkoutPlanResponse> findAll() {
+		User user = getUserAuthenticated();
+		List<WorkoutPlan> list = workoutPlanRepository.findByUserId(user.getId());
+		
+		return list.stream().map(workoutPlanMapper::toWorkoutPlanResponse).toList();
+	}
+	
+	public WorkoutPlanResponse findById(UUID id) {
+		User user = getUserAuthenticated();
+		WorkoutPlan record = workoutPlanRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Record not found"));
+		checkOwnership(record, user);
+		
+		return workoutPlanMapper.toWorkoutPlanResponse(record);
+	}
+	
+	public WorkoutPlanResponse update(UUID id, WorkoutPlanRequest request){
+		User user = getUserAuthenticated();
+		WorkoutPlan record = workoutPlanRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Record not found"));
+		checkOwnership(record, user);
+		
+		record.setName(request.recordName());
+		record.setDescription(request.description());
+		record.setStartDate(request.startDate());
+		record.setEndDate(request.endDate());
+		
+		workoutPlanRepository.save(record);
+		
+		return workoutPlanMapper.toWorkoutPlanResponse(record);
+	}
+	
+	public void deactivate(UUID id) {
+		User user = getUserAuthenticated();
+		WorkoutPlan record = workoutPlanRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Record not found"));
+		checkOwnership(record, user);
+		
+		record.setActive(false);
+		
+		workoutPlanRepository.save(record);
 	}
 }
