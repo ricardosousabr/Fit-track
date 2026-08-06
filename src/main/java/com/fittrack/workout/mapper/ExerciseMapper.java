7f7993acea17250c617d4fbf9305f258605e048d
ricardosousabr <ricardo.br.pi@gmail.com>
2026-08-05 21:21:23 -0300

    feat: "create mapper"

--- src/main/java/com/fittrack/workout/mapper/ExerciseMapper.java
+++ src/main/java/com/fittrack/workout/mapper/ExerciseMapper.java
@@ -2,10 +2,13 @@ package com.fittrack.workout.mapper;
 
 import com.fittrack.workout.domain.Exercise;
 import com.fittrack.workout.dto.ExerciseResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ExerciseMapper {
+	
 	public ExerciseResponse toExerciseResponse(Exercise exercise) {
 		return new ExerciseResponse(
 				exercise.getId(),
