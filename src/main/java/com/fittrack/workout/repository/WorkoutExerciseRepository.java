823e29d0eb0eb2ad67400c2a375f9fbba05bd7ce
ricardosousabr <ricardo.br.pi@gmail.com>
2026-08-05 21:21:07 -0300

    feat: "create function of find id"

--- src/main/java/com/fittrack/workout/repository/WorkoutExerciseRepository.java
+++ src/main/java/com/fittrack/workout/repository/WorkoutExerciseRepository.java
@@ -5,10 +5,12 @@ import com.fittrack.workout.domain.WorkoutPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WorkoutExerciseRepository extends JpaRepository<WorkoutExercise, UUID> {
 	List<WorkoutExercise> findByWorkoutPlan(WorkoutPlan workoutPlan);
 	List<WorkoutExercise> findByWorkoutPlanId(UUID workoutPlanId);
+	Optional<WorkoutExercise> findByWorkoutPlanIdAndExerciseId(UUID workoutPlanId, UUID exerciseId);
 	
 }
